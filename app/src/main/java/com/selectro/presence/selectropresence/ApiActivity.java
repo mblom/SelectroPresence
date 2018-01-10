8f9ec387b8219a4561f36f4d82ad4ccd3e6a48c6
Mark Blom <mark@markblom.nl>
2018-01-10 20:31:30 +0100

    First commit

---
+++ app/src/main/java/com/selectro/presence/selectropresence/ApiActivity.java
@@ -0,0 +1,166 @@
+package com.selectro.presence.selectropresence;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.Response;
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.io.InputStream;
+
+public class ApiActivity extends AppCompatActivity {
+    EditText etGitHubUser; // This will be a reference to our GitHub username input.
+    Button btnGetRepos;  // This is a reference to the "Get Repos" button.
+    TextView tvRepoList;  // This will reference our repo list text box.
+    ImageView imgPicure;
+
+    RequestQueue requestQueue;  // This is our requests queue to process our HTTP requests.
+
+    //String baseUrl = "http://mbl-laptop:45455/api/todoitems/";  // This is the API base URL (GitHub API)
+    String baseUrl = "http://presence.selectroweb.nl/presenceservice.svc/getuser/";  // This is the API base URL (GitHub API)
+    String uid;
+    String url;  // This will hold the full URL which will include the username entered in the etGitHubUser.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);  // This is some magic for Android to load a previously saved state for when you are switching between actvities.
+        setContentView(R.layout.activity_api);  // This links our code to our layout which we defined earlier.
+
+        this.etGitHubUser = (EditText) findViewById(R.id.et_github_user);  // Link our github user text box.
+        this.btnGetRepos = (Button) findViewById(R.id.btn_get_repos);  // Link our clicky button.
+        this.tvRepoList = (TextView) findViewById(R.id.tv_repo_list);  // Link our repository list text output box.
+        this.tvRepoList.setMovementMethod(new ScrollingMovementMethod());  // This makes our text box scrollable, for those big GitHub contributors with lots of repos :)
+
+        requestQueue = Volley.newRequestQueue(this);  // This setups up a new request queue which we will need to make HTTP requests.
+
+    }
+
+    private void clearRepoList() {
+        // This will clear the repo list (set it as a blank string).
+        this.tvRepoList.setText("");
+    }
+
+    private void addToRepoList(String uid, String lastname, String pictureurl) {
+        // This will add a new repo to our list.
+        // It combines the repoName and lastUpdated strings together.
+        // And then adds them followed by a new line (\n\n make two new lines).
+        String strRow = uid + " / " + lastname + " / " + pictureurl;
+        String currentText = tvRepoList.getText().toString();
+        this.tvRepoList.setText(currentText + "\n\n" + strRow);
+
+        new DownloadImageFromInternet((ImageView) findViewById(R.id.imgPicture))
+                .execute(pictureurl);
+    }
+
+
+    private void setRepoListText(String str) {
+        // This is used for setting the text of our repo list box to a specific string.
+        // We will use this to write a "No repos found" message if the user doens't have any.
+        this.tvRepoList.setText(str);
+    }
+
+    private void getRepoList(String uid) {
+        // First, we insert the username into the repo url.
+        // The repo url is defined in GitHubs API docs (https://developer.github.com/v3/repos/).
+        //this.url = this.baseUrl + username + "/repos";
+        this.url = this.baseUrl + uid;
+        Log.e("Url", url);
+
+        // Next, we create a new JsonArrayRequest. This will use Volley to make a HTTP request
+        // that expects a JSON Array Response.
+        // To fully understand this, I'd recommend readng the office docs: https://developer.android.com/training/volley/index.html
+        JsonRequest arrReq = new JsonObjectRequest(Request.Method.GET, url,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Check the length of our response (to see if the user has any repos)
+                        if (response.length() > 0) {
+                            try {
+                                JSONObject jsonObj = response;
+                                String uid = jsonObj.get("Uid").toString();
+                                String lastname = jsonObj.get("Displayname").toString();
+                                String pictureurl = jsonObj.get("Picture").toString();
+                                addToRepoList(uid, lastname, pictureurl);
+                            } catch (JSONException e) {
+                                // If there is an error then output this to the logs.
+                                Log.e("Volley", "Invalid JSON Object.");
+                            }
+                        } else {
+                            // The user didn't have any repos.
+                            setRepoListText("No repos found.");
+                        }
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // If there a HTTP error then add a note to our repo list.
+                        setRepoListText("Error while calling REST API");
+                        Log.e("Volley", error.toString());
+                    }
+                }
+        );
+        // Add the request we just defined to our request queue.
+        // The request queue will automatically handle the request as soon as it can.
+        requestQueue.add(arrReq);
+    }
+
+    public void getReposClicked(View v) {
+        // Clear the repo list (so we have a fresh screen to add to)
+        clearRepoList();
+        // Call our getRepoList() function that is defined above and pass in the
+        // text which has been entered into the etGitHubUser text input field.
+        getRepoList(etGitHubUser.getText().toString());
+    }
+
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView = imageView;
+            Toast.makeText(getApplicationContext(), "Please wait, it may take a few minute...", Toast.LENGTH_SHORT).show();
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL = urls[0];
+            Bitmap bimage = null;
+            try {
+                InputStream in = new java.net.URL(imageURL).openStream();
+                bimage = BitmapFactory.decodeStream(in);
+
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+                e.printStackTrace();
+            }
+            return bimage;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+    }
+}
+
